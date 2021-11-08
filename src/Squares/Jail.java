f2fae48142781ab1da506525250d33ae7ca772b1
falknielsen <emil-kvaerkeby@live.dk>
2021-11-08 15:15:42 +0100

    Rettede mapper

---
+++ src/Squares/Jail.java
@@ -0,0 +1,10 @@
+package Squares;
+
+public class Jail extends Square {
+
+    public Jail (String squareName, String squareDescription){
+        super(squareName, squareDescription);
+        this.SquareName = "Fængsel";
+        this.SquareDescription = "Gå i fængsel";
+    }
+}
---
+++ src/Squares/SquareList.java
@@ -0,0 +1,31 @@
+package Squares;
+
+public class SquareList {
+
+    public static String[][] SquareList = {
+           //  {"Start", "Modtag 2M når du passerer start"},
+            {"Burgerbaren", "M1", "grå"},
+            {"Pizzahuset", "M1", "grå"},
+           //  {"Chance", "Tag et kort"},
+            {"Godtebutikken", "M1", "lyseblå"},
+            {"Iskiosken", "M1" , "lyseblå"},
+           //  {"I fængsel", "På besøg"},
+            {"Museet", "M2", "lilla"},
+            {"Biblioteket", "M2", "lilla"},
+           //  {"Chance", "Tag et kort"},
+            {"Skaterparken", "M2", "orange"},
+            {"Svømmebassinet", "M2", "orange"},
+           //  {"Gratis parkering"},
+            {"Spillehallen", "M3", "Rød"},
+            {"Biografen", "M3", "Rød"},
+           //  {"Chance", "Tag et kort"},
+            {"Legetøjsbutikken", "M3", "gul"},
+            {"Dyrehandlen", "M3", "gul"},
+            // {"Gå i fængsel", "fængsel"},
+            {"Bowlinghallen", "M4", "grøn"},
+            {"Zoologisk have", "M4", "grøn"},
+            // {"Chance", "Tag et kort"},
+            {"Vandlandet", "M5", "blå"},
+            {"Strandpromenaden", "M5", "blå"},
+    };
+}
