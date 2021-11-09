b0ef37492d87f54aa3b305ea43e41c4bcbfbc12f
falknielsen <emil-kvaerkeby@live.dk>
2021-11-09 15:40:27 +0100

    Tilføjet landOnSquare til Jail klassen

--- 10_CDIO-del3/src/Squares/Jail.java
+++ 10_CDIO-del3/src/Squares/Jail.java
@@ -7,4 +7,11 @@ public class Jail extends Square {
         this.SquareName = "Fængsel";
         this.SquareDescription = "Gå i fængsel";
     }
+
+    public void landOnSquare(Player[] players, int player) {
+        // Rykker spiller til position 6, altså felt 7, som er fængslet.
+        // Samt udtrykker det som en boolean, så spillet ved at spilleren er i fængsel
+        players[player].setPosition(6);
+        players[player].setInJail(true);
+    }
 }
