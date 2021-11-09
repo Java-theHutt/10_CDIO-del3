af6e8eca5eabf885b6e11617e075aab92cc5850e
falknielsen <emil-kvaerkeby@live.dk>
2021-11-09 15:41:11 +0100

    Oprettet chancekort feltet

---
+++ 10_CDIO-del3/src/Squares/ChanceSquare.java
@@ -0,0 +1,17 @@
+package Squares;
+
+import java.util.Random;
+
+public class ChanceSquare extends Square {
+
+    public ChanceSquare(){
+        super("?", "Tag et chancekort");
+        // this.generateChanceCards(true);
+    }
+
+    public void landOnSquare (Player[] players, int player){
+
+    }
+
+    // Skal udvides med at trække og blande chance kort.
+}
