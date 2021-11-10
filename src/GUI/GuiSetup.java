c20d28626be08c0dabdb70a60acb1cd7f08bde12
1vernuclear <83249678+1vernuclear@users.noreply.github.com>
2021-11-10 04:50:36 +0100

    importeret Game pakkens klasser

--- src/GUI/GuiSetup.java
+++ src/GUI/GuiSetup.java
@@ -1,4 +1,19 @@
 package GUI;
 
-public class GuiSetup {
+import Game.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+//class extends the default Jframe class + implements an Actionlistener.
+//Actionlistener listens for an action, we define what the action is.
+//when the action is performed the ActionListeners default method (ActionPerformed) will run. We can override that method.
+public class GuiSetup extends JFrame implements ActionListener {
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
