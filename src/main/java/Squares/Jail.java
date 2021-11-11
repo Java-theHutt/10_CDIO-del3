4c2b18dca42f120d117310a73e998df601d35bcf
1vernuclear <83249678+1vernuclear@users.noreply.github.com>
2021-11-11 17:26:48 +0100

    Imported player class

--- src/main/java/Squares/Jail.java
+++ src/main/java/Squares/Jail.java
@@ -1,5 +1,5 @@
 package Squares;
-// import Player.Player;
+import Game.Player;
 
 public class Jail extends Square {
 
@@ -14,7 +14,7 @@ public class Jail extends Square {
         Moves the player to position 6 on the board, square 7, which is jail
         Takes 3$ from the player, and puts it on the loose change square
         */
-        players[player].setPosition(6);
+        //players[player].setPosition(6);
         // players[player].setScore(-3);
     }
 }
