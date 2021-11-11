dd544de07fc68f8c59cdb1354e93b5abce5f6807
1vernuclear <83249678+1vernuclear@users.noreply.github.com>
2021-11-11 17:16:08 +0100

    tilføjet xml

--- src/main/java/GUI/GuiSetup.java
+++ src/main/java/GUI/GuiSetup.java
@@ -2,6 +2,7 @@ package GUI;
 
 import Game.*;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,10 +11,54 @@ import java.awt.event.ActionListener;
 //when the action is performed the ActionListeners default method (ActionPerformed) will run. We can override that method.
 public class GuiSetup extends JFrame implements ActionListener {
 
+    DiceCollection diceCollection = new DiceCollection();
+    JButton button;
+    JLabel label = new JLabel();
+    JPanel panel = new JPanel();
+    ImageIcon monopoly = new ImageIcon("Monopoly.png");   //saves the imagefile in the variable monopoly
 
+    int count = 0;
+
+    public GuiSetup(){
+
+        button = new JButton("Roll Dice");
+        button.setBackground(Color.GREEN);
+        button.setFont(new Font("MV Boli",Font.BOLD,25));
+        button.setBounds(225, 250, 150, 100);
+        label.setBounds(225,10,200,100);
+        label.setFont(new Font("MV Boli",Font.BOLD,25));
+        //adds the actionslistener to button. Everytime this button is clicked the Actionlisteners method runs.
+        button.addActionListener(this);
+        button.setFocusable(false);
+
+        this.add(label);
+
+        this.add(button);
+        this.add(panel);        //adds panel to frame
+        this.setPreferredSize(new Dimension(monopoly.getIconWidth(), monopoly.getIconHeight()+30));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        //Exits the code when the frame is closed
+        this.setTitle("Monopoly");      //sets title of the frame to monopoly
+        this.pack();        //Makes the frame adjust its size to all the components, thats been implemented.
+        this.setResizable(false);
+        this.setVisible(true);        //makes the frame visible
+
+    }
+
+    public void paint(Graphics g){
+        super.paint(g);
+
+        Graphics g2D = (Graphics) g;
+
+        g2D.drawImage(monopoly.getImage(),0,30,null);
+    }
 
     @Override
+    //This method is performed when the button is pressed.
     public void actionPerformed(ActionEvent e) {
-
+        if(e.getSource() == button){
+            diceCollection.roll();
+            label.setText("You rolled " + diceCollection.getRollSum());
+            System.out.println("You rolled " + diceCollection.getRollSum());
+        }
     }
 }
