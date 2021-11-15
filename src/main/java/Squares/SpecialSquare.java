ade2ec39dd2298eaaaf5beb83a4c5432f6e89d20
falknielsen <emil-kvaerkeby@live.dk>
2021-11-15 14:41:00 +0100

    Oprettet SpecialSquare til at håndtere gratis parkering og gå i fængsel

---
+++ src/main/java/Squares/SpecialSquare.java
@@ -0,0 +1,14 @@
+package Squares;
+import Game.Player;
+
+public class SpecialSquare extends Square {
+
+    // Used for "Gå i fængsel" and "Gratis parkering"
+    public SpecialSquare(String squareName, String squareDescription){
+        super (squareName, squareDescription);
+    }
+
+    @Override
+    public void landOnSquare(Player[] players, int player) {
+    }
+}
