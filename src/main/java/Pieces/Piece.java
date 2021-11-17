a643388f32cea0233d1733aef9c6c52bf004357a
1vernuclear <83249678+1vernuclear@users.noreply.github.com>
2021-11-17 13:49:56 +0100

    Tilføjet metode til enkeltfarvet chancekort

--- src/main/java/Pieces/Piece.java
+++ src/main/java/Pieces/Piece.java
@@ -46,10 +46,50 @@ public class Piece {
             }else if(card.getCardID() == 5){
                 goOrThrowAction();
             }else if(card.getCardID() > 5 && card.getCardID() <= 8){
-
+                singleColorAction(card);
             }
         }
 
+    }
+
+    /**
+     * Takes a chancecard as parameter. Based on the chancecard ID, the fields related to the chancecard is given.
+     * @param card
+     */
+    private void singleColorAction(Card card) {
+        switch(card.getCardID()){
+            case 6:
+                moveToOneColorAction(13,14);
+                break;
+            case 7:
+                moveToOneColorAction(10,11);
+                break;
+            case 8:
+                moveToOneColorAction(4,5);
+                break;
+        }
+    }
+
+    /**
+     * Takes the 2 squarenumbers of the given color as parameter.
+     * The player then gets to choose which of the 2 squares he wants to go to.
+     * @param square1Number
+     * @param square2number
+     */
+    public void moveToOneColorAction(int square1Number,int square2number){
+        System.out.println("You pulled the card " + cards.getCurrentCard().getCardName());
+        System.out.println("Therefore you may choose between one of the 2 colored squares, that you want to go to.");
+        //System.out.print("Enter 1, to go to " + getSquareName(square1Number,0) + " or 2 to go to " + getSquareName(square2number,0) + ". Enter here: ");
+        int x;
+        while(true){
+            x = scan.nextInt();
+            if(x == 1)
+                moveToSquare(square1Number);
+            else if(x == 2)
+                moveToSquare(square2number);
+            else
+                System.out.println("Invalid input! Enter 1 or 2..!");
+        }
     }
 
     private void goOrThrowAction() {
