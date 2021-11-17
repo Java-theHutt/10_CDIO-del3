6a0789dc0957abb84e6967e004402f7badffa8d6
1vernuclear <83249678+1vernuclear@users.noreply.github.com>
2021-11-17 14:11:41 +0100

    Tilføjet doublefarve metode

--- src/main/java/Pieces/Piece.java
+++ src/main/java/Pieces/Piece.java
@@ -47,9 +47,45 @@ public class Piece {
                 goOrThrowAction();
             }else if(card.getCardID() > 5 && card.getCardID() <= 8){
                 singleColorAction(card);
+            }else if (card.getCardID() > 8 && card.getCardID() <= 12){
+                doubleColorAction(card);
             }
         }
 
+    }
+
+    private void doubleColorAction(Card card) {
+        switch (card.getCardID()) {
+            case 9 -> moveToTwoColorAction(7, 8, 22, 23);
+            case 10 -> moveToTwoColorAction(10, 11, 19, 20);
+            case 11 -> moveToTwoColorAction(4, 5, 13, 14);
+            case 12 -> moveToTwoColorAction(1, 2, 16, 17);
+        }
+    }
+
+    public void moveToTwoColorAction(int square1Number,int square2Number,int square3Number,int square4Number){
+        System.out.println("You pulled the card " + cards.getCurrentCard().getCardName());
+        //System.out.print("Therefore you may choose to go to 1 of 4 squares: - " + getSquareName(square1Number,0) + " enter 1 - "
+        //       + getSquareName(square2Number,0) + " enter 2 - " + getSquareName(square3Number,0) + " enter 3 - " + getSquareName(square4Number) + " enter 4 -");
+        int x;
+        boolean bool = true;
+        while(bool){
+            x = scan.nextInt();
+            if(x == 1) {
+                moveToSquare(square1Number);
+                bool = false;
+            }else if(x == 2) {
+                moveToSquare(square2Number);
+                bool = false;
+            }else if(x == 3) {
+                moveToSquare(square3Number);
+                bool = false;
+            }else if(x == 4) {
+                moveToSquare(square4Number);
+                bool = false;
+            }else
+                System.out.println("Invalid input! Enter 1 or 2..!");
+        }
     }
 
     /**
@@ -74,20 +110,23 @@ public class Piece {
      * Takes the 2 squarenumbers of the given color as parameter.
      * The player then gets to choose which of the 2 squares he wants to go to.
      * @param square1Number
-     * @param square2number
+     * @param square2Number
      */
-    public void moveToOneColorAction(int square1Number,int square2number){
+    public void moveToOneColorAction(int square1Number,int square2Number){
         System.out.println("You pulled the card " + cards.getCurrentCard().getCardName());
         System.out.println("Therefore you may choose between one of the 2 colored squares, that you want to go to.");
         //System.out.print("Enter 1, to go to " + getSquareName(square1Number,0) + " or 2 to go to " + getSquareName(square2number,0) + ". Enter here: ");
         int x;
-        while(true){
+        boolean bool = true;
+        while(bool){
             x = scan.nextInt();
-            if(x == 1)
+            if(x == 1) {
                 moveToSquare(square1Number);
-            else if(x == 2)
-                moveToSquare(square2number);
-            else
+                bool = false;
+            }else if(x == 2) {
+                moveToSquare(square2Number);
+                bool = false;
+            }else
                 System.out.println("Invalid input! Enter 1 or 2..!");
         }
     }
