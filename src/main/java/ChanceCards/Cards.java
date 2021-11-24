bf7078371f753910858cf6330388afcf6d8ff501
1vernuclear <83249678+1vernuclear@users.noreply.github.com>
2021-11-25 00:44:21 +0100

    Skiftet sprog

--- src/main/java/ChanceCards/Cards.java
+++ src/main/java/ChanceCards/Cards.java
@@ -43,18 +43,18 @@ public class Cards {
      * Creates all the cards with the Card constructor and adds them to the cardArray.
      */
     private void createCards(){
-        cardArray[0] = new Card(1,"moveToStart","The player moves his piece to the start square",0);
-        cardArray[6] = new Card(2,"moveBeach","The player moves to the beach",23);
-        cardArray[9] = new Card(3, "moveToSkaterpark","The player moves to the skaterpark. If it's not owned gets it for free. Otherwise pay the owner!",10);
-        cardArray[1] = new Card(4,"move5","The player moves up to 5 squares forward",5);
-        cardArray[3] = new Card(5,"goOrThrow","The player moves 1 field forward or takes another Chancecard");
-        cardArray[8] = new Card(6,"moveRed","The player moves to a red field. If it's not yet owned, the player gets the field for free. Otherwise pay the owner!");
-        cardArray[2] = new Card(7,"moveToOrange","The player moves his piece to an orange square. If it's not owned by anyone, the player gets the field for free. Otherwise pay the owner!");
-        cardArray[5] = new Card(8,"moveLightblue","The player moves to a lightblue field. If it's not yet owned the player gets the field for free. Otherwise pay the owner!");
-        cardArray[7] = new Card(9,"movePinkOrDarkblue", "The player moves to a pink or darkblue field, and gets the field for free, if it's not owned. Otherwise pay the owner!");
-        cardArray[4] = new Card(10,"moveOrangeOrGreen","The player moves to a orange or green field. If it's not owned yet the player gets it for free. Otherwise oay the owner!");
-        cardArray[10] = new Card(11,"moveLightblueOrRed","The player moves to a lightblue or red field. If it's not yet owned, gets it for free. Otherwise pay the owner!");
-        cardArray[11] = new Card(12,"moveBrownOrYellow","The player moves to a brown or yellow field. If it's not owned by anyone, gets it for free. Otherwise pay the owner!");
+        cardArray[0] = new Card(1,"Ryk frem til start!","The player moves his piece to the start square",0);
+        cardArray[6] = new Card(2,"Ryk frem til Strandpromenaden","The player moves to the beach",23);
+        cardArray[9] = new Card(3, "Ryk frem til Skaterparken","The player moves to the skaterpark. If it's not owned gets it for free. Otherwise pay the owner!",10);
+        cardArray[1] = new Card(4,"Ryk 0 - 5 felter frem","The player moves up to 5 squares forward",5);
+        cardArray[3] = new Card(5,"Ryk et felt frem eller træk nyr chancekort","The player moves 1 field forward or takes another Chancecard");
+        cardArray[8] = new Card(6,"Ryk frem til et af de røde felter","The player moves to a red field. If it's not yet owned, the player gets the field for free. Otherwise pay the owner!");
+        cardArray[2] = new Card(7,"Ryk frem til et af de orange felter","The player moves his piece to an orange square. If it's not owned by anyone, the player gets the field for free. Otherwise pay the owner!");
+        cardArray[5] = new Card(8,"Ryk frem til et lyseblåt felt","The player moves to a lightblue field. If it's not yet owned the player gets the field for free. Otherwise pay the owner!");
+        cardArray[7] = new Card(9,"Ryk frem til et lilla eller mørkeblåt felt", "The player moves to a pink or darkblue field, and gets the field for free, if it's not owned. Otherwise pay the owner!");
+        cardArray[4] = new Card(10,"Ryk frem til et orange eller grønt felt","The player moves to a orange or green field. If it's not owned yet the player gets it for free. Otherwise oay the owner!");
+        cardArray[10] = new Card(11,"Ryk frem til et lyseblåt eller rødt felt","The player moves to a lightblue or red field. If it's not yet owned, gets it for free. Otherwise pay the owner!");
+        cardArray[11] = new Card(12,"Ryk frem til et gråt eller gult felt","The player moves to a brown or yellow field. If it's not owned by anyone, gets it for free. Otherwise pay the owner!");
         //cardArray[12] = new Card(13,"candySadge","You ate to much candy. Pay M2 to the bank!");
         //cardArray[13] = new Card(14,"getOuttaJail","You are released from jail. Keep this card until you may need it.");
         //cardArray[14] = new Card(15,"birthdayPerson","It's your birthday. You receive M1 from your fellow players.");
